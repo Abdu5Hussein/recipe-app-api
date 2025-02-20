4056eeefe18177b8459312dd9071e392bfff1d11
Abdussalam Khasheba <akhisheba@gmail.com>
2025-02-20 11:28:27 +0200

    fix linting

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3.9-alpine3.13
+#FROM python:3.9-alpine3.13
+FROM python:latest
 LABEL maintainer="Abdu"
 
 ENV PYTHONUNBUFFERED 1
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,8 +2,29 @@
 django command to wait for the database to be available
 """
 from django.core.management.base import BaseCommand
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
+
 
 class Command(BaseCommand):
-    #django command to wait for the database
+    # django command to wait for the database
+
     def handle(self, *args, **options):
-        pass
+        """entry point for command"""
+        self.stdout.write("waiting for database ...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (
+                Psycopg2Error,
+                OperationalError
+            ):
+                self.stdout.write(
+                    "database is not available, waiting 1 second"
+                )
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("database available!"))
